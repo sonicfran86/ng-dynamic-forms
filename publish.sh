cc7fd4faf609501a46a46b875fbaa7f27040a4b4
sonicfran86 <julio.maqueda-ext@renaissance.com>
2021-12-13 17:10:25 +0100

    update repo

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-npm publish ./dist/@ng-dynamic-forms/core/
-npm publish ./dist/@ng-dynamic-forms/ui-basic/
-npm publish ./dist/@ng-dynamic-forms/ui-bootstrap/
-npm publish ./dist/@ng-dynamic-forms/ui-foundation/
-npm publish ./dist/@ng-dynamic-forms/ui-ionic/
-npm publish ./dist/@ng-dynamic-forms/ui-kendo/
-npm publish ./dist/@ng-dynamic-forms/ui-material/
-npm publish ./dist/@ng-dynamic-forms/ui-ng-bootstrap/
-npm publish ./dist/@ng-dynamic-forms/ui-primeng/
+npm publish ./dist/@ng-dynamic-forms/core/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-basic/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-bootstrap/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-foundation/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-ionic/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-kendo/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-material/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-ng-bootstrap/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
+npm publish ./dist/@ng-dynamic-forms/ui-primeng/ --registry https://inexus.ixxus.co.uk/repository/npm-private/
